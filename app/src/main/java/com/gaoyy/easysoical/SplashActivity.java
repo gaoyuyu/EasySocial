2ff4718f9666b0045e9d587292207976f658fea8
gaoyuyu <740514999@qq.com>
2016-02-18 10:20:27 +0800

    initEasySoical

---
+++ app/src/main/java/com/gaoyy/easysoical/SplashActivity.java
@@ -0,0 +1,34 @@
+package com.gaoyy.easysoical;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.readystatesoftware.systembartint.SystemBarTintManager;
+
+public class SplashActivity extends AppCompatActivity
+{
+    private static final long DELAY_TIME = 2000L;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        SystemBarTintManager tintManager = new SystemBarTintManager(this);
+        tintManager.setStatusBarTintResource(R.color.status_bar_color);
+        tintManager.setStatusBarTintEnabled(true);
+        new Handler().postDelayed(new Runnable()
+        {
+            @Override
+            public void run()
+            {
+                startActivity(new Intent(SplashActivity.this,
+                        MainActivity.class));
+                finish();
+                overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
+            }
+        }, DELAY_TIME);
+    }
+}
